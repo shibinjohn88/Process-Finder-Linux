adcdd510a0065cd71432b8d180d6cde41602f997
Shibin <shibinjohnpdlm@aol.com>
2020-10-11 11:38:12 -0400

    created function findTime
    function findTime will find the time before the number of days we give as argument

--- process_finder.py
+++ process_finder.py
@@ -1,3 +1,16 @@
-!#/bin/bash/python3
-import psutil
+
 import datetime
+def findTime(daysBefore):
+    # find time before the number of days in variable <Days>
+
+    #Calculate current time
+    currentTime = datetime.datetime.now()
+
+    #Finding old time based on days before
+    dateRange = datetime.timedelta(days=daysBefore)
+    pointTime = currentTime -  dateRange
+    unixTime = pointTime.timestamp()
+    return unixTime
+
+
+findTime(10)
