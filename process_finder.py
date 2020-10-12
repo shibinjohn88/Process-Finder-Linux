88beabf3d96b733e79ec98f40be8c39ebf3f3399
Shibin <shibinjohnpdlm@aol.com>
2020-10-11 23:14:05 -0400

    New function getProcess created

--- process_finder.py
+++ process_finder.py
@@ -1,5 +1,16 @@
-
+import psutil
 import datetime
+
+listOfProcessNames = []
+
+def getListOfProcesses(createTime):
+    intervalTime = findTime(createTime)
+    for pro in psutil.process_iter():
+        prInfoDict = pro.as_dict(attrs=['pid', 'name', 'create_time', 'username'])
+        if prInfoDict['create_time'] < intervalTime:
+            listOfProcessNames.append(prInfoDict)
+    return
+
 def findTime(daysBefore):
     # find time before the number of days in variable <Days>
 
@@ -12,5 +23,18 @@ def findTime(daysBefore):
     unixTime = pointTime.timestamp()
     return unixTime
 
+getListOfProcesses(1)
+# Setting default value for 1 day
 
-findTime(10)
+#Sort list by create time and PID
+listOfProcessNames = sorted(listOfProcessNames, key = lambda i: (i['create_time'], i['pid']))
+
+#Get values from list of processes
+for currentPrcoess in listOfProcessNames:
+    username = currentPrcoess["username"]
+    pid = currentPrcoess["pid"]
+    creationTime = currentPrcoess["create_time"]
+    creationTimeString = datetime.datetime.fromtimestamp(creationTime).strftime('%d.%m.%Y %H:%M:%S')
+    processName = currentPrcoess["name"]
+    print("Username: %s, PID: %8i, Program: %s" % (username, pid, processName),", created on", creationTimeString)
+
